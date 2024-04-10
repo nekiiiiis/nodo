1bc44c04b29eb68b7c375b30ca630cdef2c7406c
Neco Martinez <114484645+nekiiiiis@users.noreply.github.com>
2024-04-10 18:34:02 +0200

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import subprocess
 from openai import OpenAI
 
 class LLMServerConfig:
-    MAX_MODEL_LEN = 3100
+    MAX_MODEL_LEN = 2300
     CHAT_TEMPLATE = ("{% for message in messages %}"
                      "{{'' + message['role'] + '\n' + message['content'] + '' + '\n'}}"
                      "{% endfor %}"
